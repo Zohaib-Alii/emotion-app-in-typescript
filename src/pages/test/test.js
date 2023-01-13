6639b5ac0775037dc0dd9bf41cefe1c89a41fd9d
Zohaib Arain <zobiiiarain803@gmail.com>
2023-01-12 23:05:31 -0800

    adding new method for append new component

--- src/pages/test/test.js
+++ src/pages/test/test.js
@@ -1,7 +1,15 @@
 import React from "react";
 
 const test = () => {
-  return <div>test</div>;
+  const handleOpen = () => {
+    console.log("show ");
+  };
+  return (
+    <div>
+      test
+      <h1 onClick={handleOpen}>another component</h1>
+    </div>
+  );
 };
 
 export default test;
